cfe854f6c8559bd3e176ba3e4d10a428f403d1f6
Tyler Pohn <tylerpohn@gmail.com>
2021-12-25 10:58:50 -0600

    twoSum typescript

---
+++ twoSum.ts
@@ -0,0 +1,29 @@
+/*
+  Given an array of numbers and a target number,
+  return true if two of the numbers in the array add up to the target.
+  Otherwise, return false.
+
+  You may assume that each input would have exactly one solution, and you may not use the same element twice.
+  The straightforward way to solve this problem would take O(n²)time. Is it possible to do this in O(n) time? 
+
+  Example:
+
+  const nums = [2, 5, 11, 15]
+  twoSum(nums, 7) -> true
+  Rational:  nums[0] + nums[1] = 2 + 5 = 7,
+
+  twoSum(nums, 9) -> false
+  Rational: No elements inside the array sum up to the target number
+*/
+
+const twoSum = (arr: number[], target: number): boolean => {
+  const cache = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (cache[arr[i]]) return true;
+    const compliment = target - arr[i];
+    cache[compliment] = true;
+  }
+  return false;
+};
+
+console.log(twoSum([2, 5, 11, 15], 7));
